19c711f687a3ffe7ccaf0342347334a08c46dfee
Walter Reiner <walter.reiner@wreiner.at>
2023-05-04 21:07:23 +0200

    Cleanup defaults file

--- screens/defaults.py
+++ screens/defaults.py
@@ -1,19 +1,26 @@
+# -- WINDOW SETUP
 WINDOW_HEIGHT = 600
 WINDOW_WIDTH = 800
 WINDOW_TITLE = "01-beginnings"
+BACKGROUNG_IMG = "res/imgs/ishan-seefromthesky-6U-sSfBV-gM-unsplash.jpg"
 
+# -- COLORS
+COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255,255,255)
 COLOR_RED = (255,0,0)
 COLOR_GREEN = (0,255,0)
 COLOR_BLUE = (0,0,255)
 
+# -- PLAYER
 PLAYER_SHIP = "res/icons/space-shuttle.png"
-ENEMY_10PTS = "res/icons/fremdes-schiff.png"
-BACKGROUNG_IMG = "res/imgs/ishan-seefromthesky-6U-sSfBV-gM-unsplash.jpg"
-
-ENEMY_X_STEPS = 1
 PLAYER_X_STEPS = 5
-# ENEMY_X_STEPS = 1
+
+# -- ENEMY
+ENEMY_10PTS = "res/icons/fremdes-schiff.png"
+ENEMY_X_STEPS = 1
 ENEMY_Y_STEPS = 10
 
+# -- LASER
 LASER_Y_STEPS = 5
+LASER_WIDTH = 3
+LASER_HEIGHT = 7
